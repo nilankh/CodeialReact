302cd6604d72c8c3d6fd59b682c9d93a67239d38
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-18 15:28:51 +0530

    handling uncontrable components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.emailInputRef = React.createRef();
+        this.passwordInputRef = React.createRef();
+    }
     render() {
         return (
             <form className="login-form">
                 <span className="login-signup-header">Log In</span>
                 <div className="field">
-                    <input type="email" placeholder="Email" required />
+                    <input type="email" placeholder="Email" required ref={this.emailInputRef} />
                 </div>
                 <div className="field">
-                <input type="Password" placeholder="Password" required />
+                <input type="Password" placeholder="Password" required ref={this.passwordInputRef}/>
                 </div>
                 <div className="field">
                     <button>Log In</button>
