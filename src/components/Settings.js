46ce4446fdba46d2372da17c10f5897a86b3b6a5
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-20 17:06:07 +0530

    adding setting ui done

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,19 @@
-import React, { Component, useReducer } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Settings extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            password: '',
+            confirmPassword: '',
+            editMode: false,
+        };
+    }
     render() {
+        const {user} = this.props.auth;
+        const { editMode } = this.state;
         return (
             <div className="settings">
                 <div className="img-container">
@@ -14,9 +26,53 @@ class Settings extends Component {
                 <div className="field">
                     <div className="field-label">Email</div>
                     <div className="field-value">{user.email}</div>
-
                 </div>
-            </div>
+
+                <div className="field">
+                    <div className="field-label">name</div>
+                    {editMode ? (
+                        <input 
+                            type="text" 
+                            onChange={() => this.handleChange() }
+                            value={this.state.name } 
+                        />
+                    ) : (
+                        <div className="field-value">{user.name}</div>
+                    )}   
+                    </div>
+
+                    {editMode && (
+                        <div className="field">
+                            <div className="field-label">New password</div>
+                            <input 
+                                type="password" 
+                                onChange={() => this.handleChange() }
+                                value={this.state.password } 
+                            />
+                        </div>
+                        )}
+
+                        {editMode && (
+                        <div className="field">
+                            <div className="field-label">Confirm password</div>
+                            <input 
+                                type="password" 
+                                onChange={() => this.handleChange() }
+                                value={this.state.confirmPassword } 
+                            />
+                        </div>
+                        )}
+
+                        <div className="btn-grp">
+                          {editMode ? (
+                            <button className="button save-btn">Save</button>
+                           ) : ( 
+                            <button className="button edit-btn">Edit Profile</button> 
+                           )}  
+
+                           {editMode && <div className="go-back">Go Back</div>}
+                        </div>
+                </div>
         );
     }
 }
