41fd5f8369bdc4d0f2db01cb4981dbf19b4ced56
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-30 17:20:57 +0530

    started working on searching users:ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
+
 import { logoutUser } from '../actions/auth';
+import { searchUsers } from '../actions/search';
 
 
 
